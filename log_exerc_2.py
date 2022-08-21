c49c5c6fd328a6e1031d950e5b3ed7911644c618
Gui Regazzo <52835026+RegazzoGui@users.noreply.github.com>
2022-08-21 14:34:09 -0300

    Update log_exerc_2.py
    Finalizado

--- log_exerc_2.py
+++ log_exerc_2.py
@@ -1,15 +1,18 @@
 """
 IMPORTANTE: Python v 3.10
-Exericio 2 - Sistema para Pizzaria
 """
-#Criando a tabela com os dados
+#Listas contendo informações pertinentes
 cod = [21, 22, 23, 24, 25]
 desc = ['Napolitana', 'Margherita', 'Calabresa', 'Toscana', 'Portuguesa']
 val_pm = [20, 20, 25, 30, 30]
 val_pg = [26, 26, 32, 39, 39]
 
+
+#Inicio Main
 #reproduzindo o quadro de informações
 msg = 'Cardápio'
+msg_1 = 'Pizzaria do Aluisio G S Regazzo'
+print(msg_1.center(65))
 print(msg.center(65, '-'))
 print('|  Código   |   Descrição   |   Pizza Média   |   Pizza Grande  |\n'
       '-----------------------------------------------------------------\n'
@@ -23,22 +26,36 @@ print('-' * 65)
 #Estrutura de repetição
 tot = []
 rep = ''
+
+#Entrada de dados: sabor e tamanho
+#Entrada tamanho da pizza
 while rep != 0:
-    #Entrada de dados, sabor e tamanho
-    #Entrada tamanho da pizza
     t = ['M', 'G']
     tam = input('Qual o tamanho da pizza que deseja (M/G): ').upper()
-    if tam not in t:
-        print('TAMANHO INVÁLIDO - Por favor, digite apenas "M" ou "G"')
-        tam = input('Qual o tamanho da pizza que deseja (M/G): ').upper()
+    while True:
+        try:
+            if tam in t:
+                break
+            else:
+                print('OPÇÃO INVÁLIDO - Por favor, digite apenas "M" ou "G"')
+                tam = input('Qual o tamanho da pizza que deseja (M/G): ').upper()
+        except ValueError:
+            break
 
 
-    #Entrada nome da pizza
+#Entrada nome da pizza
+
     c_list = [21, 22, 23, 24, 25]
     c = int(input('Entre com o código do sabor desejado: '))
-    if c not in c_list:
-        print('CÓDIGO INVÁLIDO - Digite apenas os códigos presentes no cardápio!')
-        c = input('Entre com o código do sabor desejado: ')
+    while True:
+        try:
+            if c in c_list:
+                break
+            else:
+                print('OPÇÃO INVÁLIDO - Digite apenas os códigos presentes no cardápio!')
+                c = int(input('Entre com o código do sabor desejado: '))
+        except ValueError:
+            break
 
     # Indicando o sabor e o preço
     #Escolha do sabor
@@ -60,12 +77,13 @@ while rep != 0:
 
     print(f'Você pediu a pizza de {sabor}')
 
-    print('Deseja pedir mais alguma coisa?\n'
+    rep = int(input('Deseja pedir mais alguma coisa?\n'
           '1 - Sim\n'
-          '0 - Não')
-    rep = int(input())
+          '0 - Não\n'
+          '>>> '))
 
     tot.append(preco)
+
 
 total = float(sum(tot))
 print(f'O total a ser pago é: R$ {total:.2f}')
